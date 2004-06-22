3fd81bf749376a9c95ef7b8958b92ed0d128777a
Doug MacEachern <dougm@hyperic.com>
2004-06-22 06:11:38 +0000

    decls for new proc_modules functions

--- include/sigar.h
+++ include/sigar.h
@@ -276,6 +276,15 @@ typedef struct {
 SIGAR_DECLARE(int) sigar_proc_exe_get(sigar_t *sigar, sigar_pid_t pid,
                                       sigar_proc_exe_t *procexe);
 
+typedef struct {
+    void *data; /* user data */
+
+    int (*module_getter)(void *, char *, int);
+} sigar_proc_modules_t;
+
+SIGAR_DECLARE(int) sigar_proc_modules_get(sigar_t *sigar, sigar_pid_t pid,
+                                          sigar_proc_modules_t *procmods);
+
 typedef enum {
     SIGAR_FSTYPE_UNKNOWN,
     SIGAR_FSTYPE_NONE,
