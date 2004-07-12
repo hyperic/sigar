13ce45c30b390a0284813fd0379ca0e8de5b53ef
Doug MacEachern <dougm@hyperic.com>
2004-07-12 21:19:53 +0000

    bump version

--- bindings/java/src/net/hyperic/sigar/Sigar.java
+++ bindings/java/src/net/hyperic/sigar/Sigar.java
@@ -15,7 +15,7 @@ import net.hyperic.jni.ArchNotSupportedException;
  */
 public class Sigar implements SigarProxy {
 
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
     private static SigarLoader loader = new SigarLoader(Sigar.class);
     private FileSystemMap mounts = null;
