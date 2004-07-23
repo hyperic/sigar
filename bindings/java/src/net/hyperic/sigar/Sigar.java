8c9bf6414f4d943afbc5a36c9c30c0cf9f671e4a
Doug MacEachern <dougm@hyperic.com>
2004-07-23 17:36:52 +0000

    use SigarVersion

--- bindings/java/src/net/hyperic/sigar/Sigar.java
+++ bindings/java/src/net/hyperic/sigar/Sigar.java
@@ -15,7 +15,11 @@ import net.hyperic.jni.ArchNotSupportedException;
  */
 public class Sigar implements SigarProxy {
 
-    public static final String VERSION = "1.1.4";
+    public static final String VERSION_STRING =
+        SigarVersion.VERSION_STRING;
+
+    public static final String BUILD_DATE =
+        SigarVersion.BUILD_DATE;
 
     private static SigarLoader loader = new SigarLoader(Sigar.class);
     private FileSystemMap mounts = null;
