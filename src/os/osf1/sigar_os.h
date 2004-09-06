e2795350fc6ca0f0e3d800d9cc6a82f42bfdb287
Doug MacEachern <dougm@hyperic.com>
2004-09-06 03:27:28 +0000

    add includes

--- src/os/osf1/sigar_os.h
+++ src/os/osf1/sigar_os.h
@@ -1,6 +1,12 @@
 #ifndef SIGAR_OS_H
 #define SIGAR_OS_H
 
+#include <sys/vm.h>
+
+#include <mach.h>
+#include <mach/mach_types.h>
+#include <mach/vm_statistics.h>
+
 struct sigar_t {
     SIGAR_T_BASE;
 };
