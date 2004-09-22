ab39e15dbe79f5ecd12037c40b62084161825481
Doug MacEachern <dougm@hyperic.com>
2004-09-22 16:51:49 +0000

    workaround FormatSize issue w/ mono.

--- bindings/dotnet/src/Sigar.cs
+++ bindings/dotnet/src/Sigar.cs
@@ -48,13 +48,13 @@ namespace Hyperic.Sigar {
         }
 
         [DllImport(Sigar.LIBSIGAR)]
-        private static extern string
+        private static extern void
         sigar_format_size(ulong size, StringBuilder buffer);
 
         public static string FormatSize(ulong size) {
-            StringBuilder buffer = new StringBuilder();
-            buffer.Capacity = 56;
-            return sigar_format_size(size, buffer);
+            StringBuilder buffer = new StringBuilder(56);
+            sigar_format_size(size, buffer);
+            return buffer.ToString();
         }
 
         public Mem Mem() {
