54dbe31c629013a75b7708da8395a2e3f1410716
Doug MacEachern <dougm@hyperic.com>
2004-11-10 23:37:57 +0000

    ProcMem for multiple processes

---
+++ bindings/java/src/net/hyperic/sigar/MultiProcMem.java
@@ -0,0 +1,25 @@
+package net.hyperic.sigar;
+
+import net.hyperic.sigar.ptql.ProcessFinder;
+
+public class MultiProcMem extends ProcMem {
+
+    static ProcMem get(Sigar sigar, String query)
+        throws SigarException {
+
+        ProcMem mem = new ProcMem();
+
+        long[] pids = ProcessFinder.find(sigar, query);
+
+        for (int i=0; i<pids.length; i++) {
+            ProcMem pmem = sigar.getProcMem(pids[i]);
+            mem.size     += pmem.size;
+            mem.vsize    += pmem.vsize;
+            mem.resident += pmem.resident;
+            mem.share    += pmem.share;
+            mem.rss      += pmem.rss;
+        }
+        
+        return mem;
+    }
+}
