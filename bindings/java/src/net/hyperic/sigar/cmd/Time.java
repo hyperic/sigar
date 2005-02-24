8021f4de5bbcb6d9bb8ccc6015d1bda07b08f5bb
Doug MacEachern <dougm@hyperic.com>
2005-02-24 19:30:25 +0000

    print times

--- bindings/java/src/net/hyperic/sigar/cmd/Time.java
+++ bindings/java/src/net/hyperic/sigar/cmd/Time.java
@@ -2,6 +2,7 @@ package net.hyperic.sigar.cmd;
 
 import net.hyperic.sigar.Sigar;
 import net.hyperic.sigar.SigarException;
+import net.hyperic.sigar.ThreadCpu;
 
 public class Time extends SigarCommandBase {
 
@@ -26,7 +27,21 @@ public class Time extends SigarCommandBase {
     }
 
     public void output(String[] args) throws SigarException {
+        ThreadCpu cpu = new ThreadCpu();
+        long start = System.currentTimeMillis();
+
         this.shell.handleCommand("time " + args[0], args);
+
+        cpu.gather(this.sigar, 0);
+        System.out.println("real....." +
+                           (System.currentTimeMillis() - start) / 1000);
+        System.out.println("user....." + toMillis(cpu.getUser()));
+        System.out.println("sys......" + toMillis(cpu.getSys()));
+        System.out.println("total...." + toMillis(cpu.getTotal()));
+    }
+
+    private static long toMillis(long nano) {
+        return nano / 1000000;
     }
 
     public static void main(String[] args) throws Exception {
