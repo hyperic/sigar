39e76c9b4b8149f87bc43be9b6418dc21af1004f
Doug MacEachern <dougm@hyperic.com>
2005-06-15 01:52:45 +0000

    trace attrs

--- bindings/java/src/net/hyperic/sigar/test/TestProcMem.java
+++ bindings/java/src/net/hyperic/sigar/test/TestProcMem.java
@@ -20,6 +20,11 @@ public class TestProcMem extends SigarTestCase {
 
         ProcMem procMem = sigar.getProcMem(sigar.getPid());
 
+        traceln("Size=" + Sigar.formatSize(procMem.getSize()));
+        traceln("Vsize=" + Sigar.formatSize(procMem.getVsize()));
+        traceln("Resident=" + Sigar.formatSize(procMem.getResident()));
+        traceln("Share=" + Sigar.formatSize(procMem.getShare()));
+        traceln("Rss=" + Sigar.formatSize(procMem.getRss()));
         assertTrue(procMem.getSize() > 0);
         // XXX vsize, resident, share, rss
     }
