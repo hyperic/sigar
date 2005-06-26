2f1392e26485ea686303ba5dc6fbf0ab29b2c706
Doug MacEachern <dougm@hyperic.com>
2005-06-26 00:20:06 +0000

    what could be less serializable than class with 2 fields that are pointers to C structures

--- bindings/java/src/net/hyperic/sigar/win32/Service.java
+++ bindings/java/src/net/hyperic/sigar/win32/Service.java
@@ -2,8 +2,7 @@ package net.hyperic.sigar.win32;
 
 import java.util.List;
 
-public class Service extends Win32Bindings implements java.io.Serializable
-{
+public class Service extends Win32Bindings {
     // Service State
     public static final int SERVICE_STOPPED          = 0x00000001;
     public static final int SERVICE_START_PENDING    = 0x00000002;
