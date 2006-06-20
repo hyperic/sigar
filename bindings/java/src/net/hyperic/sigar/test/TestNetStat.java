6ff7de288c6e8c0cafd431b93f47369bd7ccd8f2
Doug MacEachern <dougm@hyperic.com>
2006-06-20 01:01:45 +0000

    dont print listeners by default

--- bindings/java/src/net/hyperic/sigar/test/TestNetStat.java
+++ bindings/java/src/net/hyperic/sigar/test/TestNetStat.java
@@ -7,7 +7,9 @@ import net.hyperic.sigar.Sigar;
 import net.hyperic.sigar.SigarNotImplementedException;
 
 public class TestNetStat extends SigarTestCase {
-    
+    private static boolean printListeners =
+        "true".equals(System.getProperty("sigar.netstat.listeners"));
+
     public TestNetStat(String name) {
         super(name);
     }
@@ -21,10 +23,16 @@ public class TestNetStat extends SigarTestCase {
         } catch (SigarNotImplementedException e) {
             return;
         }
-        
+
+        traceln("");
         assertGtEqZeroTrace("Outbound", netstat.getTcpOutboundTotal());
         assertGtEqZeroTrace("Inbound", netstat.getTcpInboundTotal());
         assertGtEqZeroTrace("Listen", netstat.getTcpListen());
+        assertGtEqZeroTrace("Established", netstat.getTcpEstablished());
+
+        if (!printListeners) {
+            return;
+        }
 
         int flags = NetFlags.CONN_SERVER | NetFlags.CONN_TCP;
 
