6662294a7f679c31b12b1f10003cf28d02f625f1
Bjoern Martin <boris59@boris59.de>
2007-05-01 20:26:42 +0000

    fixed formatting (tabs to spaces, version from 1.4 to 1.5, general formatting) added MBean for "LoadAverage"

--- bindings/java/src/org/hyperic/sigar/jmx/SigarMem.java
+++ bindings/java/src/org/hyperic/sigar/jmx/SigarMem.java
@@ -35,238 +35,217 @@ import org.hyperic.sigar.SigarException;
  * package. Provides an OpenMBean conform implementation.
  * 
  * @author Bjoern Martin
- * @since 1.4 (2007-04)
+ * @since 1.5
  */
 public class SigarMem extends AbstractMBean {
 
-	private static final String MBEAN_TYPE = "Mem";
+    private static final String MBEAN_TYPE = "Mem";
 
-	private static final MBeanInfo MBEAN_INFO;
-	
-	private static final MBeanAttributeInfo MBEAN_ATTR_ACTUAL_FREE;
+    private static final MBeanInfo MBEAN_INFO;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_ACTUAL_USED;
+    private static final MBeanAttributeInfo MBEAN_ATTR_ACTUAL_FREE;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_FREE;
+    private static final MBeanAttributeInfo MBEAN_ATTR_ACTUAL_USED;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_RAM;
+    private static final MBeanAttributeInfo MBEAN_ATTR_FREE;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_TOTAL;
+    private static final MBeanAttributeInfo MBEAN_ATTR_RAM;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_USED;
+    private static final MBeanAttributeInfo MBEAN_ATTR_TOTAL;
 
-	private static final MBeanConstructorInfo MBEAN_CONSTR_SIGAR;
+    private static final MBeanAttributeInfo MBEAN_ATTR_USED;
 
-	private static MBeanParameterInfo MBEAN_PARAM_SIGAR;
-	
-	static {
-		MBEAN_ATTR_ACTUAL_FREE = new MBeanAttributeInfo(
-				"ActualFree", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_ATTR_ACTUAL_USED = new MBeanAttributeInfo(
-				"ActualUsed", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_ATTR_FREE = new MBeanAttributeInfo(
-				"Free", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_ATTR_RAM = new MBeanAttributeInfo(
-				"Ram", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_ATTR_TOTAL = new MBeanAttributeInfo(
-				"Total", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_ATTR_USED = new MBeanAttributeInfo(
-				"Used", "long",
-				"TODO add proper description here",
-				true, false, false);
-		MBEAN_PARAM_SIGAR = new MBeanParameterInfo(
-				"sigar", Sigar.class.getName(),
-				"The Sigar instance to use to fetch data from");
-		MBEAN_CONSTR_SIGAR = new MBeanConstructorInfo(
-				SigarMem.class.getName(),
-				"Creates a new instance, using the Sigar instance " +
-				"specified to fetch the data.",
-				new MBeanParameterInfo[]{
-						MBEAN_PARAM_SIGAR});
-		MBEAN_INFO = new MBeanInfo(
-				SigarMem.class.getName(),
-				"Sigar Memory MBean, provides raw data for the physical " +
-				"memory installed on the system. Uses an internal cache " +
-				"that invalidates within 500ms, allowing for bulk request " +
-				"being satisfied with a single dataset fetch.",
-				new MBeanAttributeInfo[]{
-					MBEAN_ATTR_ACTUAL_FREE, 
-					MBEAN_ATTR_ACTUAL_USED, 
-					MBEAN_ATTR_FREE, 
-					MBEAN_ATTR_RAM, 
-					MBEAN_ATTR_TOTAL, 
-					MBEAN_ATTR_USED}, 
-				new MBeanConstructorInfo[]{
-					MBEAN_CONSTR_SIGAR},
-					null, null);
-		
-	}
-	
-	/**
-	 * Object name this instance will give itself when being registered to an
-	 * MBeanServer.
-	 */
-	private final String objectName;
+    private static final MBeanConstructorInfo MBEAN_CONSTR_SIGAR;
 
-	/**
-	 * Creates a new instance, using the Sigar instance specified to fetch the
-	 * data.
-	 * 
-	 * @param sigar
-	 *            The Sigar instance to use to fetch data from
-	 * 
-	 * @throws IllegalArgumentException
-	 *             If an unexpected Sigar error occurs
-	 */
-	public SigarMem(Sigar sigar) throws IllegalArgumentException {
-		super(sigar, CACHED_500MS);
+    private static MBeanParameterInfo MBEAN_PARAM_SIGAR;
 
-		this.objectName = SigarInvokerJMX.DOMAIN_NAME + ":" + MBEAN_ATTR_TYPE
-				+ "=Memory";
-	}
+    static {
+        MBEAN_ATTR_ACTUAL_FREE = new MBeanAttributeInfo("ActualFree", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_ATTR_ACTUAL_USED = new MBeanAttributeInfo("ActualUsed", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_ATTR_FREE = new MBeanAttributeInfo("Free", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_ATTR_RAM = new MBeanAttributeInfo("Ram", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_ATTR_TOTAL = new MBeanAttributeInfo("Total", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_ATTR_USED = new MBeanAttributeInfo("Used", "long",
+                "TODO add proper description here", true, false, false);
+        MBEAN_PARAM_SIGAR = new MBeanParameterInfo("sigar", Sigar.class
+                .getName(), "The Sigar instance to use to fetch data from");
+        MBEAN_CONSTR_SIGAR = new MBeanConstructorInfo(SigarMem.class.getName(),
+                "Creates a new instance, using the Sigar instance "
+                        + "specified to fetch the data.",
+                new MBeanParameterInfo[] { MBEAN_PARAM_SIGAR });
+        MBEAN_INFO = new MBeanInfo(
+                SigarMem.class.getName(),
+                "Sigar Memory MBean, provides raw data for the physical "
+                        + "memory installed on the system. Uses an internal cache "
+                        + "that invalidates within 500ms, allowing for bulk request "
+                        + "being satisfied with a single dataset fetch.",
+                new MBeanAttributeInfo[] { MBEAN_ATTR_ACTUAL_FREE,
+                        MBEAN_ATTR_ACTUAL_USED, MBEAN_ATTR_FREE,
+                        MBEAN_ATTR_RAM, MBEAN_ATTR_TOTAL, MBEAN_ATTR_USED },
+                new MBeanConstructorInfo[] { MBEAN_CONSTR_SIGAR }, null, null);
 
-	/**
-	 * Object name this instance will give itself when being registered to an
-	 * MBeanServer.
-	 */
-	public String getObjectName() {
-		return this.objectName;
-	}
+    }
 
-	/**
-	 * @return The actual amount of free physical memory, in [bytes]
-	 */
-	public long getActualFree() {
-		try {
-			return sigar.getMem().getActualFree();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * Object name this instance will give itself when being registered to an
+     * MBeanServer.
+     */
+    private final String objectName;
 
-	/**
-	 * @return The actual amount of physical memory used, in [bytes]
-	 */
-	public long getActualUsed() {
-		try {
-			return sigar.getMem().getActualUsed();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * Creates a new instance, using the Sigar instance specified to fetch the
+     * data.
+     * 
+     * @param sigar
+     *            The Sigar instance to use to fetch data from
+     * 
+     * @throws IllegalArgumentException
+     *             If an unexpected Sigar error occurs
+     */
+    public SigarMem(Sigar sigar) throws IllegalArgumentException {
+        super(sigar, CACHED_500MS);
 
-	/**
-	 * @return The amount of free physical memory, in [bytes]
-	 */
-	public long getFree() {
-		try {
-			return sigar.getMem().getFree();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+        this.objectName = SigarInvokerJMX.DOMAIN_NAME + ":" + MBEAN_ATTR_TYPE
+                + "=Memory";
+    }
 
-	/**
-	 * @return The amount of physical memory, in [bytes]
-	 */
-	public long getRam() {
-		try {
-			return sigar.getMem().getRam();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * Object name this instance will give itself when being registered to an
+     * MBeanServer.
+     */
+    public String getObjectName() {
+        return this.objectName;
+    }
 
-	/**
-	 * @return The total amount of physical memory, in [bytes]
-	 */
-	public long getTotal() {
-		try {
-			return sigar.getMem().getTotal();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * @return The actual amount of free physical memory, in [bytes]
+     */
+    public long getActualFree() {
+        try {
+            return sigar.getMem().getActualFree();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-	/**
-	 * @return The amount of physical memory in use, in [bytes]
-	 */
-	public long getUsed() {
-		try {
-			return sigar.getMem().getUsed();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * @return The actual amount of physical memory used, in [bytes]
+     */
+    public long getActualUsed() {
+        try {
+            return sigar.getMem().getActualUsed();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-	// -------
-	// Implementation of the DynamicMBean interface
-	// -------
+    /**
+     * @return The amount of free physical memory, in [bytes]
+     */
+    public long getFree() {
+        try {
+            return sigar.getMem().getFree();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.management.DynamicMBean#getAttribute(java.lang.String)
-	 */
-	public Object getAttribute(String attr) throws AttributeNotFoundException,
-			MBeanException, ReflectionException {
-		if (MBEAN_ATTR_ACTUAL_FREE.getName().equals(attr)) {
-			return new Long(getActualFree());
+    /**
+     * @return The amount of physical memory, in [bytes]
+     */
+    public long getRam() {
+        try {
+            return sigar.getMem().getRam();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else if (MBEAN_ATTR_ACTUAL_USED.getName().equals(attr)) {
-			return new Long(getActualUsed());
+    /**
+     * @return The total amount of physical memory, in [bytes]
+     */
+    public long getTotal() {
+        try {
+            return sigar.getMem().getTotal();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else if (MBEAN_ATTR_FREE.getName().equals(attr)) {
-			return new Long(getFree());
+    /**
+     * @return The amount of physical memory in use, in [bytes]
+     */
+    public long getUsed() {
+        try {
+            return sigar.getMem().getUsed();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else if (MBEAN_ATTR_RAM.getName().equals(attr)) {
-			return new Long(getRam());
+    // -------
+    // Implementation of the DynamicMBean interface
+    // -------
 
-		} else if (MBEAN_ATTR_TOTAL.getName().equals(attr)) {
-			return new Long(getTotal());
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.management.DynamicMBean#getAttribute(java.lang.String)
+     */
+    public Object getAttribute(String attr) throws AttributeNotFoundException,
+            MBeanException, ReflectionException {
+        if (MBEAN_ATTR_ACTUAL_FREE.getName().equals(attr)) {
+            return new Long(getActualFree());
 
-		} else if (MBEAN_ATTR_USED.getName().equals(attr)) {
-			return new Long(getUsed());
+        } else if (MBEAN_ATTR_ACTUAL_USED.getName().equals(attr)) {
+            return new Long(getActualUsed());
 
-		} else {
-			throw new AttributeNotFoundException(attr);
-		}
-	}
+        } else if (MBEAN_ATTR_FREE.getName().equals(attr)) {
+            return new Long(getFree());
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#setAttribute(javax.management.Attribute)
-	 */
-	public void setAttribute(Attribute attr) throws AttributeNotFoundException {
-		throw new AttributeNotFoundException(attr.getName());
-	}
+        } else if (MBEAN_ATTR_RAM.getName().equals(attr)) {
+            return new Long(getRam());
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#invoke(java.lang.String,
-	 *      java.lang.Object[], java.lang.String[])
-	 */
-	public Object invoke(String actionName, Object[] params, String[] signature)
-			throws ReflectionException {
-		throw new ReflectionException(new NoSuchMethodException(actionName),
-				actionName);
-	}
+        } else if (MBEAN_ATTR_TOTAL.getName().equals(attr)) {
+            return new Long(getTotal());
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#getMBeanInfo()
-	 */
-	public MBeanInfo getMBeanInfo() {
-		return MBEAN_INFO;
-	}
+        } else if (MBEAN_ATTR_USED.getName().equals(attr)) {
+            return new Long(getUsed());
+
+        } else {
+            throw new AttributeNotFoundException(attr);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#setAttribute(javax.management.Attribute)
+     */
+    public void setAttribute(Attribute attr) throws AttributeNotFoundException {
+        throw new AttributeNotFoundException(attr.getName());
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#invoke(java.lang.String,
+     *      java.lang.Object[], java.lang.String[])
+     */
+    public Object invoke(String actionName, Object[] params, String[] signature)
+            throws ReflectionException {
+        throw new ReflectionException(new NoSuchMethodException(actionName),
+                actionName);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#getMBeanInfo()
+     */
+    public MBeanInfo getMBeanInfo() {
+        return MBEAN_INFO;
+    }
 }
--- bindings/java/src/org/hyperic/sigar/jmx/SigarSwap.java
+++ bindings/java/src/org/hyperic/sigar/jmx/SigarSwap.java
@@ -35,174 +35,165 @@ import org.hyperic.sigar.SigarException;
  * package. Provides an OpenMBean conform implementation.
  * 
  * @author Bjoern Martin
- * @since 1.4 (2007-04)
+ * @since 1.5
  */
 public class SigarSwap extends AbstractMBean {
 
-	private static final String MBEAN_TYPE = "Swap";
+    private static final String MBEAN_TYPE = "Swap";
 
-	private static final MBeanInfo MBEAN_INFO;
-	
-	private static final MBeanAttributeInfo MBEAN_ATTR_FREE;
+    private static final MBeanInfo MBEAN_INFO;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_TOTAL;
+    private static final MBeanAttributeInfo MBEAN_ATTR_FREE;
 
-	private static final MBeanAttributeInfo MBEAN_ATTR_USED;
+    private static final MBeanAttributeInfo MBEAN_ATTR_TOTAL;
 
-	private static final MBeanConstructorInfo MBEAN_CONSTR_SIGAR;
+    private static final MBeanAttributeInfo MBEAN_ATTR_USED;
 
-	private static MBeanParameterInfo MBEAN_PARAM_SIGAR;
-	
-	static {
-		MBEAN_ATTR_FREE = new MBeanAttributeInfo(
-				"Free", "long",
-				"The amount of free swap memory, in [bytes]",
-				true, false, false);
-		MBEAN_ATTR_TOTAL = new MBeanAttributeInfo(
-				"Total", "long",
-				"The total amount of swap memory, in [bytes]",
-				true, false, false);
-		MBEAN_ATTR_USED = new MBeanAttributeInfo(
-				"Used", "long",
-				"The amount of swap memory in use, in [bytes]",
-				true, false, false);
-		MBEAN_PARAM_SIGAR = new MBeanParameterInfo(
-				"sigar", Sigar.class.getName(),
-				"The Sigar instance to use to fetch data from");
-		MBEAN_CONSTR_SIGAR = new MBeanConstructorInfo(
-				SigarSwap.class.getName(),
-				"Creates a new instance, using the Sigar instance " +
-				"specified to fetch the data.",
-				new MBeanParameterInfo[]{
-						MBEAN_PARAM_SIGAR});
-		MBEAN_INFO = new MBeanInfo(
-				SigarSwap.class.getName(),
-				"Sigar Swap MBean, provides raw data for the swap memory " +
-				"configured on the system. Uses an internal cache that " +
-				"invalidates within 5 seconds.",
-				new MBeanAttributeInfo[]{
-					MBEAN_ATTR_FREE, 
-					MBEAN_ATTR_TOTAL, 
-					MBEAN_ATTR_USED}, 
-				new MBeanConstructorInfo[]{
-					MBEAN_CONSTR_SIGAR},
-					null, null);
-		
-	}
-	
-	/**
-	 * Object name this instance will give itself when being registered to an
-	 * MBeanServer.
-	 */
-	private final String objectName;
+    private static final MBeanConstructorInfo MBEAN_CONSTR_SIGAR;
 
-	/**
-	 * Creates a new instance, using the Sigar instance specified to fetch the
-	 * data.
-	 * 
-	 * @param sigar
-	 *            The Sigar instance to use to fetch data from
-	 * 
-	 * @throws IllegalArgumentException
-	 *             If an unexpected Sigar error occurs
-	 */
-	public SigarSwap(Sigar sigar) throws IllegalArgumentException {
-		super(sigar, CACHED_5SEC);
+    private static MBeanParameterInfo MBEAN_PARAM_SIGAR;
 
-		this.objectName = SigarInvokerJMX.DOMAIN_NAME + ":" + MBEAN_ATTR_TYPE
-				+ "=Swap";
-	}
+    static {
+        MBEAN_ATTR_FREE = new MBeanAttributeInfo("Free", "long",
+                "The amount of free swap memory, in [bytes]", true, false,
+                false);
+        MBEAN_ATTR_TOTAL = new MBeanAttributeInfo("Total", "long",
+                "The total amount of swap memory, in [bytes]", true, false,
+                false);
+        MBEAN_ATTR_USED = new MBeanAttributeInfo("Used", "long",
+                "The amount of swap memory in use, in [bytes]", true, false,
+                false);
+        MBEAN_PARAM_SIGAR = new MBeanParameterInfo("sigar", Sigar.class
+                .getName(), "The Sigar instance to use to fetch data from");
+        MBEAN_CONSTR_SIGAR = new MBeanConstructorInfo(
+                SigarSwap.class.getName(),
+                "Creates a new instance, using the Sigar instance "
+                        + "specified to fetch the data.",
+                new MBeanParameterInfo[] { MBEAN_PARAM_SIGAR });
+        MBEAN_INFO = new MBeanInfo(
+                SigarSwap.class.getName(),
+                "Sigar Swap MBean, provides raw data for the swap memory "
+                        + "configured on the system. Uses an internal cache that "
+                        + "invalidates within 5 seconds.",
+                new MBeanAttributeInfo[] { MBEAN_ATTR_FREE, MBEAN_ATTR_TOTAL,
+                        MBEAN_ATTR_USED },
+                new MBeanConstructorInfo[] { MBEAN_CONSTR_SIGAR }, null, null);
 
-	/**
-	 * Object name this instance will give itself when being registered to an
-	 * MBeanServer.
-	 */
-	public String getObjectName() {
-		return this.objectName;
-	}
+    }
 
-	/**
-	 * @return The amount of free swap memory, in [bytes]
-	 */
-	public long getFree() {
-		try {
-			return sigar.getSwap().getFree();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * Object name this instance will give itself when being registered to an
+     * MBeanServer.
+     */
+    private final String objectName;
 
-	/**
-	 * @return The total amount of swap memory, in [bytes]
-	 */
-	public long getTotal() {
-		try {
-			return sigar.getSwap().getTotal();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+    /**
+     * Creates a new instance, using the Sigar instance specified to fetch the
+     * data.
+     * 
+     * @param sigar
+     *            The Sigar instance to use to fetch data from
+     * 
+     * @throws IllegalArgumentException
+     *             If an unexpected Sigar error occurs
+     */
+    public SigarSwap(Sigar sigar) throws IllegalArgumentException {
+        super(sigar, CACHED_5SEC);
 
-	/**
-	 * @return The amount of swap memory in use, in [bytes]
-	 */
-	public long getUsed() {
-		try {
-			return sigar.getSwap().getUsed();
-		} catch (SigarException e) {
-			throw unexpectedError(MBEAN_TYPE, e);
-		}
-	}
+        this.objectName = SigarInvokerJMX.DOMAIN_NAME + ":" + MBEAN_ATTR_TYPE
+                + "=Swap";
+    }
 
-	// -------
-	// Implementation of the DynamicMBean interface
-	// -------
+    /**
+     * Object name this instance will give itself when being registered to an
+     * MBeanServer.
+     */
+    public String getObjectName() {
+        return this.objectName;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.management.DynamicMBean#getAttribute(java.lang.String)
-	 */
-	public Object getAttribute(String attr) throws AttributeNotFoundException,
-			MBeanException, ReflectionException {
-		if (MBEAN_ATTR_FREE.getName().equals(attr)) {
-			return new Long(getFree());
+    /**
+     * @return The amount of free swap memory, in [bytes]
+     */
+    public long getFree() {
+        try {
+            return sigar.getSwap().getFree();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else if (MBEAN_ATTR_TOTAL.getName().equals(attr)) {
-			return new Long(getTotal());
+    /**
+     * @return The total amount of swap memory, in [bytes]
+     */
+    public long getTotal() {
+        try {
+            return sigar.getSwap().getTotal();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else if (MBEAN_ATTR_USED.getName().equals(attr)) {
-			return new Long(getUsed());
+    /**
+     * @return The amount of swap memory in use, in [bytes]
+     */
+    public long getUsed() {
+        try {
+            return sigar.getSwap().getUsed();
+        } catch (SigarException e) {
+            throw unexpectedError(MBEAN_TYPE, e);
+        }
+    }
 
-		} else {
-			throw new AttributeNotFoundException(attr);
-		}
-	}
+    // -------
+    // Implementation of the DynamicMBean interface
+    // -------
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#setAttribute(javax.management.Attribute)
-	 */
-	public void setAttribute(Attribute attr) throws AttributeNotFoundException {
-		throw new AttributeNotFoundException(attr.getName());
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.management.DynamicMBean#getAttribute(java.lang.String)
+     */
+    public Object getAttribute(String attr) throws AttributeNotFoundException,
+            MBeanException, ReflectionException {
+        if (MBEAN_ATTR_FREE.getName().equals(attr)) {
+            return new Long(getFree());
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#invoke(java.lang.String,
-	 *      java.lang.Object[], java.lang.String[])
-	 */
-	public Object invoke(String actionName, Object[] params, String[] signature)
-			throws ReflectionException {
-		throw new ReflectionException(new NoSuchMethodException(actionName),
-				actionName);
-	}
+        } else if (MBEAN_ATTR_TOTAL.getName().equals(attr)) {
+            return new Long(getTotal());
 
-	/*
-	 * (non-Javadoc)
-	 * @see javax.management.DynamicMBean#getMBeanInfo()
-	 */
-	public MBeanInfo getMBeanInfo() {
-		return MBEAN_INFO;
-	}
+        } else if (MBEAN_ATTR_USED.getName().equals(attr)) {
+            return new Long(getUsed());
+
+        } else {
+            throw new AttributeNotFoundException(attr);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#setAttribute(javax.management.Attribute)
+     */
+    public void setAttribute(Attribute attr) throws AttributeNotFoundException {
+        throw new AttributeNotFoundException(attr.getName());
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#invoke(java.lang.String,
+     *      java.lang.Object[], java.lang.String[])
+     */
+    public Object invoke(String actionName, Object[] params, String[] signature)
+            throws ReflectionException {
+        throw new ReflectionException(new NoSuchMethodException(actionName),
+                actionName);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.management.DynamicMBean#getMBeanInfo()
+     */
+    public MBeanInfo getMBeanInfo() {
+        return MBEAN_INFO;
+    }
 }
