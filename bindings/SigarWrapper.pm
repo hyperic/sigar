4a504e6cf44738c3599752cefa02bbbc3cc03c82
Doug MacEachern <dougm@hyperic.com>
2007-09-03 19:36:03 +0000

    move archname to SigarWrapper

--- bindings/SigarWrapper.pm
+++ bindings/SigarWrapper.pm
@@ -2,6 +2,7 @@
 package SigarWrapper;
 
 use strict;
+use Config;
 use Cwd;
 use Exporter;
 use File::Path;
@@ -9,6 +10,52 @@ use IO::File ();
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(generate);
+
+sub archname {
+    my $os = lc $^O;
+    my $vers = $Config{osvers};
+    my $arch = $Config{archname};
+
+    if ($os =~ /win32/) {
+        return 'x86-winnt';
+    }
+    elsif ($os =~ /linux/) {
+        return 'x86-linux';
+    }
+    elsif ($os =~ /hpux/) {
+        if ($vers =~ /11\./) {
+            return 'pa-hpux-11';
+        }
+    }
+    elsif ($os =~ /aix/) {
+        return 'ppc-aix-5';
+    }
+    elsif ($os =~ /solaris/) {
+        if ($arch =~ /sun4/) {
+            return 'sparc-solaris';
+        }
+        elsif ($arch =~ /.86/) {
+            return 'x86-solaris';
+        }
+    }
+    elsif ($os =~ /darwin/) {
+        return 'universal-macosx';
+    }
+    elsif ($os =~ /freebsd/) { 
+        if($arch =~ /.86/) { 
+            if($vers =~ /6\../ ) { 
+                return 'x86-freebsd-6'; 
+            }
+        } 
+        elsif( $arch =~ /amd64/) { 
+            if($vers =~ /6\../ ) { 
+                return 'amd64-freebsd-6'; 
+            } 
+        } 
+    } 
+
+    return '';
+}
 
 my %platforms = (
     A => "AIX",
