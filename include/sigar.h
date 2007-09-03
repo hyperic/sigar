75f2f90b0767dbaccb4a2305dce3c3be895cfb4b
Doug MacEachern <dougm@hyperic.com>
2007-09-03 16:48:58 +0000

    add NULL_HWADDR constant

--- include/sigar.h
+++ include/sigar.h
@@ -536,6 +536,8 @@ SIGAR_DECLARE(int) sigar_net_route_list_destroy(sigar_t *sigar,
 #define SIGAR_IFF_MULTICAST   0x800
 #define SIGAR_IFF_SLAVE       0x1000
 
+#define SIGAR_NULL_HWADDR "00:00:00:00:00:00"
+
 typedef struct {
     char name[16];
     char type[64];
