2fd3beab062b7022c904db964ea6f10387328729
Doug MacEachern <dougm@hyperic.com>
2007-11-13 05:38:37 +0000

    remove reflection around regex classes

--- bindings/java/src/org/hyperic/sigar/ptql/StringPattern.java
+++ bindings/java/src/org/hyperic/sigar/ptql/StringPattern.java
@@ -18,55 +18,26 @@
 
 package org.hyperic.sigar.ptql;
 
-import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.hyperic.sigar.util.ReferenceMap;
 
 public class StringPattern {
-    static Class patternClass;
-    static Method compile, matcher, find;
-    private static Map patterns = null;
+    private static Map patterns = ReferenceMap.synchronizedMap();
     
-    //XXX avoiding 1.4 requirement
-    private static void init() throws Exception {
-        patterns = ReferenceMap.synchronizedMap();
-        patternClass = Class.forName("java.util.regex.Pattern");
-        Class matcherClass = Class.forName("java.util.regex.Matcher");
-        Class[] arg = { String.class };
-        compile = patternClass.getDeclaredMethod("compile", arg);
-        arg = new Class[] { Class.forName("java.lang.CharSequence") };
-        matcher = patternClass.getDeclaredMethod("matcher", arg);
-        find = matcherClass.getDeclaredMethod("find", new Class[0]);
-    }
-
     /**
      * Wrapper around Pattern.compile(regex).matcher(source).find()
      */
     public static boolean matches(String source, String regex) {
-        try {
-            if (patterns == null) {
-                init();
-            }
-
-            Object pattern = patterns.get(regex);
-            if (pattern == null) {
-                //pattern = Pattern.compile(regex)
-                pattern = compile.invoke(patternClass, new Object[] { regex });
-                patterns.put(regex, pattern);
-            }
-            //m = pattern.matcher(source)
-            Object m = matcher.invoke(pattern, new Object[] { source });
-            //result = m.find()
-            Boolean result = (Boolean)find.invoke(m, new Object[0]);
-            return result.booleanValue();
-        } catch (Exception e) {
-            System.err.println("Error matching '" +
-                               regex + "' against '" +
-                               source + "'");
-            e.printStackTrace();
-            return false;
+        Pattern pattern = (Pattern)patterns.get(regex);
+        if (pattern == null) {
+            pattern = Pattern.compile(regex);
+            patterns.put(regex, pattern);
         }
+        Matcher matcher = pattern.matcher(source);
+        return matcher.find();
     }
 }
 
