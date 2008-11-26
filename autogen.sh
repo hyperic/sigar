8537ee876c65d3bbdef79b1d87e5a27264e1da82
Doug MacEachern <dougm@hyperic.com>
2008-11-26 23:40:31 +0000

    check for glibtoolize

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,14 @@
 set -x
 
 touch INSTALL NEWS AUTHORS
+libtoolize="libtoolize"
+if which glibtoolize >/dev/null 2>&1
+then
+    libtoolize=glibtoolize
+fi
 
 autoheader \
 && aclocal \
-&& libtoolize --ltdl --copy --force \
+&& $libtoolize --copy --force \
 && automake --add-missing --copy \
 && autoconf
