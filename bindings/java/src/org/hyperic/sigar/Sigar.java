6819b18af6eda100a9e7e4ec97e964a7cd32b193
Doug MacEachern <dougm@hyperic.com>
2009-03-25 14:13:56 -0700

    dont require pdh.dll on NT 4.0

--- bindings/java/src/org/hyperic/sigar/Sigar.java
+++ bindings/java/src/org/hyperic/sigar/Sigar.java
@@ -156,15 +156,19 @@ public class Sigar implements SigarProxy {
     }
 
     private static void loadLibrary() throws SigarException {
-        try {
-            if (SigarLoader.IS_WIN32 &&
-                System.getProperty("os.version").equals("4.0"))
-            {
+        if (SigarLoader.IS_WIN32 &&
+            System.getProperty("os.version").equals("4.0"))
+        {
+            try {
+                //NT 4.0 only
                 String lib =
                     loader.findJarPath("pdh.dll") +
                     File.separator + "pdh.dll";
                 loader.systemLoad(lib);
-            }
+            } catch (Exception e) {}
+        }
+
+        try {
             loader.load();
         } catch (ArchNotSupportedException e) {
             throw new SigarException(e.getMessage());
