ce06f8629208bb7e653c81582abd755e87fad432
Doug MacEachern <dougm@hyperic.com>
2009-07-18 16:26:52 -0700

    ruby version of Version.java

---
+++ bindings/ruby/examples/version.rb
@@ -0,0 +1,23 @@
+require 'rbsigar'
+require 'rbconfig'
+
+puts "Sigar version......." + Sigar::VERSION
+puts "Build date.........." + Sigar::BUILD_DATE
+puts "SCM rev............." + Sigar::SCM_REVISION
+
+sys = Sigar.new.sys_info
+puts "OS description......" + sys.description
+puts "OS name............." + sys.name
+puts "OS arch............." + sys.arch
+puts "OS machine.........." + sys.machine
+puts "OS version.........." + sys.version
+puts "OS patch level......" + sys.patch_level
+puts "OS vendor..........." + sys.vendor
+puts "OS vendor version..." + sys.vendor_version
+if (sys.vendor_code_name != nil)
+    puts "OS code name........" + sys.vendor_code_name
+end
+
+puts "Ruby version........" + RUBY_VERSION
+puts "Ruby build vendor..." + ::Config::CONFIG['build_vendor']
+puts "Ruby archdir........" + ::Config::CONFIG['archdir']
