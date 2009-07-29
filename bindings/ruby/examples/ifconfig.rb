807aa955c192f0be6add66a27e8be5830117a136
Matthew Kent <mkent@magoazul.com>
2009-07-28 23:18:11 -0700

    No stats for aliased devices.

--- bindings/ruby/examples/ifconfig.rb
+++ bindings/ruby/examples/ifconfig.rb
@@ -37,31 +37,32 @@ iflist.each do |ifname|
     " MTU:" + ifconfig.mtu.to_s +
     " Metric:" + ifconfig.metric.to_s
 
-  ifstat = sigar.net_interface_stat(ifname)
+  if (!ifname.include?(":"))
+    ifstat = sigar.net_interface_stat(ifname)
+    puts "\t" +
+        "RX packets:" + ifstat.rx_packets.to_s +
+        " errors:" + ifstat.rx_errors.to_s +
+        " dropped:" + ifstat.rx_dropped.to_s +
+        " overruns:" + ifstat.rx_overruns.to_s +
+        " frame:" + ifstat.rx_frame.to_s
 
-  puts "\t" +
-      "RX packets:" + ifstat.rx_packets.to_s +
-      " errors:" + ifstat.rx_errors.to_s +
-      " dropped:" + ifstat.rx_dropped.to_s +
-      " overruns:" + ifstat.rx_overruns.to_s +
-      " frame:" + ifstat.rx_frame.to_s
+    puts "\t" +
+        "TX packets:" + ifstat.tx_packets.to_s +
+        " errors:" + ifstat.tx_errors.to_s +
+        " dropped:" + ifstat.tx_dropped.to_s +
+        " overruns:" + ifstat.tx_overruns.to_s +
+        " carrier:" + ifstat.tx_carrier.to_s
 
-  puts "\t" +
-      "TX packets:" + ifstat.tx_packets.to_s +
-      " errors:" + ifstat.tx_errors.to_s +
-      " dropped:" + ifstat.tx_dropped.to_s +
-      " overruns:" + ifstat.tx_overruns.to_s +
-      " carrier:" + ifstat.tx_carrier.to_s
+    puts "\t" + "collisions:" + ifstat.tx_collisions.to_s
 
-  puts "\t" + "collisions:" + ifstat.tx_collisions.to_s
+    rx_bytes = ifstat.rx_bytes
+    tx_bytes = ifstat.tx_bytes
 
-  rx_bytes = ifstat.rx_bytes
-  tx_bytes = ifstat.tx_bytes
-
-  print "\t" +
-    "RX bytes:" + rx_bytes.to_s +
-    " (" + Sigar.format_size(rx_bytes) + ")" +
-    "  " +
-    "TX bytes:" + tx_bytes.to_s +
-    " (" + Sigar.format_size(tx_bytes) + ")" + "\n";
+    print "\t" +
+      "RX bytes:" + rx_bytes.to_s +
+      " (" + Sigar.format_size(rx_bytes) + ")" +
+      "  " +
+      "TX bytes:" + tx_bytes.to_s +
+      " (" + Sigar.format_size(tx_bytes) + ")" + "\n";
+  end
 end
