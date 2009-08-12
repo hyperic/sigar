22c1b065dd0064fbe7a1e47671589c813444ec27
Doug MacEachern <dougm@hyperic.com>
2009-08-12 10:11:36 -0700

    add IFF_MASTER to NetFlags

--- bindings/java/src/org/hyperic/sigar/NetFlags.java
+++ bindings/java/src/org/hyperic/sigar/NetFlags.java
@@ -97,6 +97,11 @@ public class NetFlags {
 
     public final static int IFF_SLAVE = 0x1000;
 
+    /**
+     * Master of a load balancer
+     */
+    public static final int IFF_MASTER = 0x2000;
+
     public static final int RTF_UP = 0x1;
 
     public static final int RTF_GATEWAY = 0x2;
