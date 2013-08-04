f15aa84dfee04b3bebd8686fe25007cd662b9744
nettag <netta@vmware.com>
2013-08-04 16:41:36 +0300

    Fixed TestProcDiskIO
    Added bytesTotal and some more verification.

--- bindings/java/src/org/hyperic/sigar/test/TestProcDiskIO.java
+++ bindings/java/src/org/hyperic/sigar/test/TestProcDiskIO.java
@@ -18,21 +18,35 @@ public class TestProcDiskIO extends SigarTestCase {
             procDiskIO = sigar.getProcDiskIO(pid);
         } catch (SigarException e) {
             traceln("pid " + pid + ": " + e.getMessage());
+            // throw e;
             return;
         }
 
+        long bytesRead = procDiskIO.getBytesRead();
+        long bytesWritten = procDiskIO.getBytesWritten();
+        long bytesTotal = procDiskIO.getBytesTotal();
+        
         traceln("Pid=" + pid);
-        traceln("Bytes Read=" + Sigar.formatSize(procDiskIO.getBytesRead()));
-        traceln("Bytes Written=" + Sigar.formatSize(procDiskIO.getBytesWritten()));
+        traceln("Bytes Read=" + Sigar.formatSize(bytesRead));
+        traceln("Bytes Written=" + Sigar.formatSize(bytesWritten));
+        traceln("Bytes Total=" + Sigar.formatSize(bytesTotal));
+        
+        if (bytesRead != -1 && bytesWritten != -1 && bytesTotal != -1) {
+        	assertTrue("Bytes total should equal bytesRead + bytesWritten",
+        			    (bytesTotal == bytesRead + bytesWritten));
+        }
     }
 
     public void testCreate() throws Exception {
         Sigar sigar = getSigar();
 
+        boolean caughtException = false;
         try {
             sigar.getProcDiskIO(getInvalidPid());
         } catch (SigarException e) {
+        	caughtException = true;
         }
+        assertTrue("Test on invalid PID should have thrown an exception.", caughtException);
 
         long[] pids = sigar.getProcList();
         for (int i=0; i<pids.length; i++) {
