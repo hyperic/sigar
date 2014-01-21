af5683b473dc837bf6ed8569badf7e53889d7378
Trond Norbye <trond.norbye@gmail.com>
2014-01-21 10:58:55 +0100

    Fix function prototypes
    Change-Id: I74596b77f24612aba065a3e862e9cf4e381969df

--- include/sigar_getline.h
+++ include/sigar_getline.h
@@ -9,10 +9,10 @@ SIGAR_DECLARE(char *) sigar_getline(char *prompt);
 SIGAR_DECLARE(void) sigar_getline_setwidth(int width);
 SIGAR_DECLARE(void) sigar_getline_redraw(void);
 SIGAR_DECLARE(void) sigar_getline_reset(void);
-SIGAR_DECLARE(void) sigar_getline_windowchanged();
+SIGAR_DECLARE(void) sigar_getline_windowchanged(void);
 SIGAR_DECLARE(void) sigar_getline_histinit(char *file);
 SIGAR_DECLARE(void) sigar_getline_histadd(char *buf);
-SIGAR_DECLARE(int)  sigar_getline_eof();
+SIGAR_DECLARE(int)  sigar_getline_eof(void);
 SIGAR_DECLARE(void) sigar_getline_completer_set(sigar_getline_completer_t func);
 
 #endif /* SIGAR_GETLINE_H */
